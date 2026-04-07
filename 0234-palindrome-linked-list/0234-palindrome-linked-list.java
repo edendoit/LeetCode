acc33c42d65dbb787d700ad2c678a8ba90bb8394
edendoit <104994876+edendoit@users.noreply.github.com>
2026-04-07 23:21:28 +0900

    Time: 3 ms (99.9%), Space: 94.5 MB (60.32%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -9,27 +9,59 @@
  * }
  */
 class Solution {
-    public boolean isPalindrome(ListNode head) {
-        ListNode p1 = head;
-        ListNode p2 = null;
+    // VERSION 1 : Time O(n) / Space O(n)
+    // public boolean isPalindrome(ListNode head) {
+    //     ListNode p1 = head;
+    //     ListNode p2 = null;
         
-        while(head != null) {
-            ListNode temp = new ListNode(head.val);
-            temp.next = p2;
-            p2 = temp;
-            head = head.next;
+    //     while(head != null) {
+    //         ListNode temp = new ListNode(head.val);
+    //         temp.next = p2;
+    //         p2 = temp;
+    //         head = head.next;
+    //     }
+    //     while(p1 != null && p2 != null) {
+    //         if(p1.val == p2.val){
+    //             p1 = p1.next;
+    //             p2 = p2.next;
+    //         } else {
+    //             return false;
+    //         }
+    //     }
+    //     if(p1 != null) return false;
+    //     if(p2 != null) return false;
+
+    //     return true;
+    // }
+
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        while(p1 != null && p2 != null) {
-            if(p1.val == p2.val){
+        
+        ListNode prev = null;
+        while(slow != null) {
+            ListNode temp = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = temp;     
+        }
+
+        ListNode p1 = head;
+        ListNode p2 = prev;
+        while(p2 != null) {
+            if(p1.val == p2.val) {
                 p1 = p1.next;
                 p2 = p2.next;
-            } else {
-                return false;
-            }
+            } else return false;
         }
-        if(p1 != null) return false;
-        if(p2 != null) return false;
+
 
         return true;
+
     }
 }
